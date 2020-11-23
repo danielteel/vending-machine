c2a3c4069e8e150ee59bc817fa668888a2f8e392
Daniel Teel <daniel.teel.2@us.af.mil>
2020-11-23 14:57:27 -0700

    Added functionality to allow user to insert bills and only a certain set of bills

--- src/Machine.js
+++ src/Machine.js
@@ -13,6 +13,18 @@ class Machine {
     }
     this.snacks = inventory
   }
+
+  deposit(rupees){
+    let validDenoms=[10, 20, 50, 100, 500]
+    if(validDenoms.includes(rupees)){
+      return `You have deposited Rs ${rupees}`;
+    }else{
+      throw Error(`INVALID BILL`);
+    }
+
+    
+    
+  }
 }
 
 module.exports = Machine
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,50 +1,62 @@
 const Machine = require('../src/Machine')
 
+let testMachine=null;
+
+beforeEach(()=>{
+  testMachine = new Machine();
+  return testMachine;
+})
+
 describe('The vending machine', () => {
+
+
   it('is initialized with no items', () => {
     // SEAT
     // setup
-    const vendingMachine = new Machine();
 
     // exercise & assert
-    expect(vendingMachine.seeSelections()).toEqual([])
+    expect(testMachine.seeSelections()).toEqual([])
 
     // teardown, not needed
   })
 
   it('can stock one snack', () => {
     // setup
-    const vendingMachine = new Machine();
     const snack = {
       name: 'macadamia nuts',
       price: 250
     }
 
     // exercise
-    vendingMachine.stock([snack])
+    testMachine.stock([snack])
 
     // assert
-    expect(vendingMachine.seeSelections()).toEqual([snack])
+    expect(testMachine.seeSelections()).toEqual([snack])
   })
-
-  // someone attempts to stock without inventory
-  // it('stocks nothing if there is no inventory passed', () => {
-  //   // setup
-  //   const vendingMachine = new Machine()
-
-  //   // exercise
-  //   vendingMachine.stock()
-
-  //   // assertion
-  //   expect(vendingMachine.seeSelections()).toEqual([])
-  // })
 
   it('displays an error if no inventory comes with stocking', () => {
     // setup
-    const vendingMachine = new Machine()
     const displayMessage = "please do not troll. you cannot stock nothing."
 
     // exercise & assert
-    expect(() => vendingMachine.stock()).toThrow(displayMessage)
+    expect(() => testMachine.stock()).toThrow(displayMessage)
   })
+  
+  it('When user inserts money running total is displayed', () =>{
+    //SEAT
+    let denoms=[10, 20, 50, 100, 500]
+    denoms.forEach(item =>{
+      expect(() => testMachine.deposit(item).toEqual(`You have deposited Rs ${item}`))
+    })
+  })
+
+  it('Display error if user inputs anything but 10/20/50/100/500 bills', () =>{
+    //SEAT
+    let denoms=[5, 15, 25, 5000];
+    denoms.forEach(item =>{
+      expect(() => testMachine.deposit(item)).toThrow(`INVALID BILL`);
+    });
+  })
+
+
 })
