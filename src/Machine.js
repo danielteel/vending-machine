e59d0a9c4f1f075ba1b8db609e26ea673342bf55
Daniel Teel <daniel.teel.2@us.af.mil>
2020-11-23 16:01:40 -0700

    Added selectItem returning an object with change

--- src/Machine.js
+++ src/Machine.js
@@ -7,9 +7,24 @@ class Machine {
   selectItem(selection) {
     let selectedSnack = this.snacks.find(item => item.name===selection);
     if (selectedSnack!==undefined){
-      if (selectedSnack.price > this._deposited){
+      
+      if (selectedSnack.price > this._deposited){ 
         return `Your deposit is insufficient.  Please add Rs ${selectedSnack.price-this._deposited} for this item`;
       }
+      
+      let denoms=[10, 20, 50, 100, 500].reverse();
+      let changeBack=[];
+      this._deposited-=selectedSnack.price;
+      while (this._deposited>10){
+        for (let bill of denoms){
+          while (bill<=this._deposited){
+            changeBack.push(bill);
+            this._deposited-=bill;
+          }
+        }
+      }
+
+      return {item:selectedSnack.name,change:changeBack}
     }else{
       return 'The item you selected is unavailable';
     }
